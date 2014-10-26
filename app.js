98b92cd154bfa549eb3e53877ec4aec0e55f974b
lehaoson <lehaoson@gmail.com>
2014-10-27 00:51:10 +0700

    Installed jade, stylus & other necessary modules.

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-var app = require('koa')();
+var koa = require('koa');
+var app = koa();
 
-app.use(function* (next) {
-    this.body = "Hello World !!!";
-    yield next;
+var path = require('path');
+var livereload = require('koa-livereload');
+var router = require('koa-router');
+var views = require('co-views');
+var serve = require('koa-static');
+var stylus = require('koa-stylus');
 
-    this.body += " I'm Cloud le!";
-});
+var render = views(__dirname + "/views", {ext: 'jade'});
+app.use(stylus(path.join(__dirname, 'public')));
+app.use(serve(__dirname + '/public'));
+app.use(router(app));
+app.use(livereload());
+
+app.get('/users/:id', users);
+
+function *users(next) {
+    this.body = yield render('user', {userId: this.params.id});
+}
+
+//app.use(function* (next) {
+//    this.body = "Hello World !!!";
+//    yield next;
+//
+//    this.body += " I'm Cloud le!";
+//});
 
 app.listen(3000);
